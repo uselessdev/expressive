8e7db7c6c5e73997b5ee1dabf29852cc92943386
Wallace Batista <wallacebatistaoliveira@gmail.com>
2016-11-21 17:10:45 -0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 'use strict'
 
 const app = require('./bootstrap/app')
-const port = process.env.PORT || 3030
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
   console.log('Server running on http://localhost:%s', port)
