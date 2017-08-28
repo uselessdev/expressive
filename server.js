88510afcf67a72f21b2ffef55af8ed82a17ec8c8
uselessdev <wallacebatistaoliveira@gmail.com>
2017-08-28 15:20:24 -0300

    Change server.js

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ require('app-module-path/register')
 const app = require('bootstrap/app')
 const port = require('config/app').port
 
-app.listen(port, () => {
-  console.log('Server running on http://localhost:%s', port)
-})
+app.listen(port, () =>
+  console.log(`Server running on http://localhost:${port}`)
+)
