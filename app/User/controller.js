68d8013d09854a5a430a004c6a1016c270f504f7
Wallace Batista <wallace.batista@bangboo.com.br>
2017-09-08 12:38:29 -0300

    Fixed error on fetchAll

--- app/User/controller.js
+++ app/User/controller.js
@@ -5,7 +5,7 @@ const User = require('./model')
 
 function index (request, response) {
   User.fetchAll()
-    .then(users => response.status(200).json({users: users.toJSON()})
+    .then(users => response.status(200).json({users: users.toJSON()}))
     .catch(error => response.status(500).send(error))
 }
 
