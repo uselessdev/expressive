c673313dbae1b1a27489b546f0ba5baad3e9450f
uselessdev <wallacebatistaoliveira@gmail.com>
2018-02-11 01:13:05 -0200

    chore: add dependecies

--- bootstrap/app.js
+++ bootstrap/app.js
@@ -1,9 +1,20 @@
 /**
  * Start Application
  */
+const helmet = require('helmet')
 const consign = require('consign')
 const express = require('express')
+const bodyParser = require('body-parser')
+const validate = require('express-validator')
+
 const app = express()
+
+app.use(helmet())
+app.use(validate())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 consign({locale: 'pt-br'})
   .include('app/Routes.js')
