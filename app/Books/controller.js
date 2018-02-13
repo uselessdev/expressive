44eb4bf4855db17daecdad279477d0f63b16bb8d
uselessdev <wallacebatistaoliveira@gmail.com>
2018-02-13 16:09:16 -0200

    books: refactor controller
    Add responses handlers

--- app/Books/controller.js
+++ app/Books/controller.js
@@ -3,31 +3,40 @@
  */
 const Books = require('./repository')
 const BooksTransform = require('./transform')
+const {
+  responseWithOk,
+  responseWithCreated,
+  responseWithDeleted
+} = require('../Responses')
 
 const index = (request, response) => {
   Books.find()
     .then(BooksTransform())
-    .then(books => response.json(books))
+    .then(responseWithOk(response))
 }
 
 const show = (request, response) => {
-  Books.findOne(request.params.id)
-    .then(book => response.json({data: book}))
+  Books.findOne(parseInt(request.params.id))
+    .then(BooksTransform())
+    .then(responseWithOk(response))
 }
 
 const store = (request, response) => {
   Books.create(request.body)
-    .then(books => response.json({data: books}))
+    .then(BooksTransform())
+    .then(responseWithCreated(response))
 }
 
 const update = (request, response) => {
-  Books.update(request.params.id)
-    .then(books => response.json(books))
+  Books.update(parseInt(request.params.id))
+    .then(BooksTransform())
+    .then(responseWithOk(response))
 }
 
 const destroy = (request, response) => {
   Books.remove(request.params.id)
-    .then(books => response.json(books))
+    .then(BooksTransform())
+    .then(responseWithDeleted(response))
 }
 
 module.exports = {
