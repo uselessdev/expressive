2c34283446873e432baacd2bdd42fb87c6552d00
uselessdev <wallacebatistaoliveira@gmail.com>
2018-02-13 15:48:32 -0200

    books: refactor controller
    Move all methods to handle with data to
    repository

--- app/Books/controller.js
+++ app/Books/controller.js
@@ -1,79 +1,31 @@
 /**
  * Books Controller
  */
-let books = [
-  {
-    id: 1,
-    title: 'The Game of Thrones - A Song of Ice and Fire - Book 1',
-    author: 'George R. R. Martin',
-    value: 16.9
-  },
-  {
-    id: 2,
-    title: 'A Clash of Kings - A Song of Ice and Fire - Book 2',
-    author: 'George R. R. Martin',
-    value: 17.2
-  },
-  {
-    id: 3,
-    title: 'A Storm of Swords - A Song of Ice and Fire - Book 3',
-    author: 'George R. R. Martin',
-    value: 20.2
-  },
-  {
-    id: 4,
-    title: 'A Feast for Crowns - A Song of Ice and Fire - Book 4',
-    author: 'George R. R. Martin',
-    value: 23.8
-  },
-  {
-    id: 5,
-    title: 'A Dance with Dragons - A Song of Ice and Fire - Book 5',
-    author: 'George R. R. Martin',
-    value: 24
-  }
-]
+const Books = require('./repository')
 
 const index = (request, response) => {
-  return response.json({
-    data: books
-  })
+  Books.find()
+    .then(books => response.json({data: books}))
 }
 
 const show = (request, response) => {
-  const { id } = request.params
-  const book = books.find(book => book.id === parseInt(id))
-
-  return response.json({
-    data: book
-  })
+  Books.findOne(request.params.id)
+    .then(book => response.json({data: book}))
 }
 
 const store = (request, response) => {
-  const book = request.body
-
-  books.push({
-    id: books.length + 1,
-    ...book
-  })
-
-  return response.json(books)
+  Books.create(request.body)
+    .then(books => response.json({data: books}))
 }
 
 const update = (request, response) => {
-  const { id } = request.params
-  const book = books.find(b => b.id === parseInt(id))
-
-  // mutation
-  Object.assign(book, request.body)
-
-  return response.json(books)
+  Books.update(request.params.id)
+    .then(books => response.json(books))
 }
 
 const destroy = (request, response) => {
-  const { id } = request.params
-  books = books.filter(book => book.id !== parseInt(id))
-  return response.json(books)
+  Books.remove(request.params.id)
+    .then(books => response.json(books))
 }
 
 module.exports = {
---
+++ app/Books/repository.js
@@ -0,0 +1,68 @@
+/**
+ * Books Repository
+ */
+let books = [
+  {
+    id: 1,
+    title: 'The Game of Thrones - A Song of Ice and Fire - Book 1',
+    author: 'George R. R. Martin',
+    value: 16.9
+  },
+  {
+    id: 2,
+    title: 'A Clash of Kings - A Song of Ice and Fire - Book 2',
+    author: 'George R. R. Martin',
+    value: 17.2
+  },
+  {
+    id: 3,
+    title: 'A Storm of Swords - A Song of Ice and Fire - Book 3',
+    author: 'George R. R. Martin',
+    value: 20.2
+  },
+  {
+    id: 4,
+    title: 'A Feast for Crowns - A Song of Ice and Fire - Book 4',
+    author: 'George R. R. Martin',
+    value: 23.8
+  },
+  {
+    id: 5,
+    title: 'A Dance with Dragons - A Song of Ice and Fire - Book 5',
+    author: 'George R. R. Martin',
+    value: 24
+  }
+]
+
+const find = () => Promise.resolve(books)
+
+const findOne = id => Promise.resolve(books.find(book => book.id === id))
+
+const create = book => {
+  books.push({
+    id: books.length + 1,
+    ...book
+  })
+
+  return Promise.resolve(books)
+}
+
+const update = (id, data) => {
+  const book = books.find(b => b.id === id)
+  Object.assign(book, data)
+
+  return Promise.resolve(books)
+}
+
+const remove = id => {
+  books = books.filter(b => b.id !== id)
+  return Promise.resolve(books)
+}
+
+module.exports = {
+  find,
+  findOne,
+  create,
+  update,
+  remove
+}
