80cd80902b31aaaee4ca9cf84ce937021007fcaf
uselessdev <wallacebatistaoliveira@gmail.com>
2018-02-19 07:19:32 -0300

    books: update controller

--- app/Books/controller.js
+++ app/Books/controller.js
@@ -2,12 +2,13 @@
  * Books Controller
  */
 const Books = require('./repository')
-const Controller = require('../Controller')(Books)
+const { Controller } = require('@anarklab/expressive-controller')
+const BookControoler = Controller(Books)
 
 module.exports = {
-  index: Controller.index,
-  show: Controller.show,
-  store: Controller.store,
-  update: Controller.update,
-  destroy: Controller.remove
+  index: BookControoler.index,
+  show: BookControoler.show,
+  store: BookControoler.store,
+  update: BookControoler.save,
+  destroy: BookControoler.remove
 }
