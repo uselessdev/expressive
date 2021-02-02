5992697c07a6ca54f50628f402dc61717649fb10
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2021-02-02 09:41:15 -0300

    chore: upgrade dependencies

--- config/app.js
+++ config/app.js
@@ -4,5 +4,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
   env: process.env.NODE_ENV || 'production',
-  locale: process.env.APP_LOCALE || 'en_us'
+  locale: process.env.APP_LOCALE || 'en-us'
 }
